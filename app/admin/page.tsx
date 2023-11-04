b75a27c317d24df8a3f17a4787a0f8550427a6a2
syndg <sinchandsgpt9@gmail.com>
2023-11-05 03:24:02 +0530

    Prisma Initialisation Basic data modelling

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
-import { UserButton, SignOutButton, currentUser } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { getUser } from "@/lib/getUser";
 
 export default async function Admin() {
+  const user = await getUser();
   return (
     <div>
       <h1>Admin</h1>
+      <p>Hello {user.name}</p>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
---
+++ prisma/migrations/20231104130740_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
+);
