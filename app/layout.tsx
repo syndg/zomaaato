a3a3c649882d6a79e6ae1a87ab1464326af1c58a
syndg <sinchandsgpt9@gmail.com>
2023-11-07 02:56:27 +0530

    Add NextJS Toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import NextTopLoader from "nextjs-toploader";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={poppins.className}>{children}</body>
+        <body className={cn(poppins.className)}>
+          <NextTopLoader color="rgb(239, 79, 95)" showSpinner={false} />
+          <div className="max-w-[1200px] mx-auto">{children}</div>
+        </body>
       </html>
     </ClerkProvider>
   );
