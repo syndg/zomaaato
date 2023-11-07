9c716242815fb60704d685eabeabc498c5ac32b3
syndg <sinchandsgpt9@gmail.com>
2023-11-07 18:56:14 +0530

    Delete unnecessary migrations

--- prisma/migrations/20231104130740_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "userId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
-);
--- prisma/migrations/20231106194549_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `imageUrl` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "imageUrl" SET NOT NULL;
