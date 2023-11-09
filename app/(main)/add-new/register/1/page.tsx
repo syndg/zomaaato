1c99363a2dacb47937877b64fcf47afa058bcf4e
syndg <sinchandsgpt9@gmail.com>
2023-11-10 01:09:44 +0530

    FormStep1

---
+++ app/(main)/add-new/register/1/page.tsx
@@ -0,0 +1,5 @@
+import Formstep1 from "@/components/FormStep1";
+
+export default function Page() {
+  return <Formstep1 />;
+}
---
+++ components/FormStep1.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Formstep1 = () => {
+  return (
+    <>
+      <h1>Form Step 1</h1>
+    </>
+  );
+};
+
+export default Formstep1;
