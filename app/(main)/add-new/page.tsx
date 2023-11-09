7a2f0eb247d3b2a3ac469483e66403f3d13db9c9
syndg <sinchandsgpt9@gmail.com>
2023-11-10 01:09:13 +0530

    Update add-new page

--- app/(main)/add-new/page.tsx
+++ app/(main)/add-new/page.tsx
@@ -1,4 +1,37 @@
+import { getUserFromDb } from "@/actions/getUserFromDb";
+import { PlusCircle } from "lucide-react";
+import { db } from "@/lib/db";
+import Link from "next/link";
+
 export default async function AddNew() {
+  const user = await getUserFromDb();
+  const id = user?.id;
+
+  const restaurants = await db.restaurant.findMany({
+    where: {
+      ownerId: id,
+    },
+  });
+
+  if (restaurants.length === 0) {
+    return (
+      <div className="h-[80vh] grid place-content-center gap-5 text-gray-700 font-semibold">
+        <p className="md:text-2xl sm:text-xl text-lg">
+          You do not have any restaurants listed.
+        </p>
+        <div className="flex justify-center">
+          <Link
+            href="/add-new/register/1"
+            className="flex items-center gap-2 bg-zomato-red hover:bg-zomato-red/90 py-2 px-3 text-white sm:text-xl text-md rounded-lg transition-colors duration-200"
+          >
+            Add new
+            <PlusCircle size={22} />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Admin</h1>
