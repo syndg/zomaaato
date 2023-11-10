dc7e4c00c1c72fc1ea6249904eb2ec72915f9631
syndg <sinchandsgpt9@gmail.com>
2023-11-11 02:38:35 +0530

    Formstep1: Basic Structure

--- app/(main)/add-new/register/1/page.tsx
+++ app/(main)/add-new/register/1/page.tsx
@@ -1,5 +1,13 @@
 import Formstep1 from "@/components/FormStep1";
+import { db } from "@/lib/db";
 
-export default function Page() {
-  return <Formstep1 />;
+export default async function Page() {
+  // Dummy data
+  const restaurants = await db.restaurant.findUnique({
+    where: {
+      id: "hello",
+    },
+  });
+
+  return <Formstep1 initialValues={restaurants} />;
 }
--- components/FormStep1.tsx
+++ components/FormStep1.tsx
@@ -1,9 +1,106 @@
 "use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
-const Formstep1 = () => {
+const formSchema = z.object({
+  name: z.string().min(3).max(30),
+  description: z.string().max(120).optional(),
+  lat: z.string(),
+  lng: z.string(),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+interface Formstep1Props {
+  initialValues: FormValues | null;
+}
+
+const Formstep1 = ({ initialValues }: Formstep1Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues || {
+      name: "",
+      description: "",
+      lat: "",
+      lng: "",
+    },
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <h1>Form Step 1</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <input placeholder="Category name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <textarea
+                    placeholder="Write a yummy description about your restaurant!"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lat"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Latitude</FormLabel>
+                <FormControl>
+                  <input placeholder="latitude" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lng"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Longitude</FormLabel>
+                <FormControl>
+                  <input placeholder="longitude" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </Form>
     </>
   );
 };
