11dc28bab6f2059ad28f82de8fe8c8e68f183154
syndg <sinchandsgpt9@gmail.com>
2023-11-12 02:24:04 +0530

    Input
    Add custom input component.

---
+++ components/ui/input.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "p-1 w-full border border-gray-400/80 rounded-md disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
