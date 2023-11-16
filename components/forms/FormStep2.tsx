07890b4b3339a11a9e5fbf42a07fde916b096c63
syndg <sinchandsgpt9@gmail.com>
2023-11-17 02:03:00 +0530

    FormStep2: Initial commit

--- components/forms/FormStep2.tsx
+++ components/forms/FormStep2.tsx
@@ -1,8 +1,159 @@
-const FormStep2 = () => {
+"use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { RestaurantTypeTimings } from "@prisma/client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormDescription,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { resCategories } from "@/lib/constants";
+
+const formSchema = z.object({
+  type: z.enum(["DINEIN", "DELIVERY", "BOTH"], {
+    required_error: "You need to select a type.",
+  }),
+  categories: z.array(z.string()).max(2, {
+    message: "You can select up to 2 categories",
+  }),
+  cuisines: z
+    .array(z.string())
+    .max(2, {
+      message: "You can select up to 2 cuisines",
+    })
+    .refine((cuisines) => cuisines.some((cuisine) => cuisine), {
+      message: "You need to select at least 1 cuisine",
+    }),
+  timingSlots: z.array(z.string()),
+  days: z.array(z.string()),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+interface FormStep2Props {
+  resId: string;
+  initialValues: RestaurantTypeTimings | null;
+}
+
+const FormStep2 = ({ resId, initialValues }: FormStep2Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues || {
+      type: "BOTH",
+      cuisines: [],
+      timingSlots: [],
+      categories: [],
+      days: [],
+    },
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Form Step 2</h1>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 p-5">
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Establishment Type</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="BOTH" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Both, delivery and dine-in available
+                    </FormLabel>
+                  </FormItem>
+
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="DELIVERY" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Delivery only</FormLabel>
+                  </FormItem>
+
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="DINEIN" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Dine-in only</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="categories"
+          render={() => (
+            <FormItem>
+              <div className="mb-4">
+                <FormLabel className="text-base">Categories</FormLabel>
+                <FormDescription>Select up to 2 best choices.</FormDescription>
+              </div>
+              <div className="grid grid-cols-3 gap-2">
+                {resCategories.map((category) => (
+                  <FormField
+                    key={category}
+                    control={form.control}
+                    name="categories"
+                    render={({ field }) => {
+                      return (
+                        <FormItem
+                          key={category}
+                          className="flex items-center gap-2"
+                        >
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value?.includes(category)}
+                              onCheckedChange={(checked: boolean) => {
+                                return checked
+                                  ? field.onChange([...field.value, category])
+                                  : field.onChange(
+                                      field.value?.filter(
+                                        (value) => value !== category,
+                                      ),
+                                    );
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal">
+                            {category}
+                          </FormLabel>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                ))}
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 };
 
---
+++ lib/constants.ts
@@ -0,0 +1,33 @@
+export const resCategories = [
+  "Bakery",
+  "Bar",
+  "Beverage Shop",
+  "Bhojanalya",
+  "Butcher Shop",
+  "Café",
+  "Casual",
+  "Dining",
+  "Club",
+  "Cocktail Bar",
+  "Confectionery",
+  "Dessert Parlour",
+  "Dhaba",
+  "Fine Dining",
+  "Food Court",
+  "Food Truck",
+  "Irani Cafe",
+  "Kiosk",
+  "Lounge",
+  "Mess",
+  "Microbrewery",
+  "Paan Shop",
+  "Pub",
+  "Quick Bites",
+  "Shack",
+  "Sweet Shop",
+] as const;
+
+const cuisineData =
+  "Afghan African American Andhra Arabian Armenian Asian Asian Fusion Assamese Australian Awadhi Bakery Bangladeshi BarFood BBQ Belgian Bengali Beverages Bihari Biryani Bohri Brazilian British BubbleTea Burger Burmese Cafe CafeFood Cake Cantonese CharcoalChicken Chettinad Chili Chinese Coffee CoffeeandTea Continental Desserts DrinksOnly Egyptian Ethiopian European FastFood FingerFood French FriedChicken FrozenYogurt Fusion Garhwali German Goan Greek Grill Gujarati HealthyFood Himachali Hotdogs Hyderabadi IceCream Indian Indonesian Iranian Irish Israeli Italian Japanese Juices Kashmiri Kebab Kerala Konkan Korean Lebanese Lucknowi Maharashtrian Malaysian Malwani Mandi Mangalorean Mediterranean Mexican Middle Eastern Mishti Mithai Modern Indian Momos Mongolian Moroccan Mughlai Naga Nepalese NorthEastern NorthIndian Odia Oriental Paan Pakistani PanAsian Panini Parsi Peruvian Pizza Poké Portuguese Rajasthani RawMeats RoastChicken Rolls Russian Salad Sandwich Seafood Sindhi Singaporean SouthAmerican SouthIndian Spanish SriLankan Steak StreetFood Sushi Swedish Tamil Tea Tex-Mex Thai Tibetan Turkish Vietnamese Wraps";
+
+export const cuisines = cuisineData.split(" ");
