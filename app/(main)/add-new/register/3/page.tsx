a57c945de2ec4e8a7418feedd45f61768c0f0f03
syndg <sinchandsgpt9@gmail.com>
2023-11-20 04:43:22 +0530

    FormStep3: Initial Commit

---
+++ app/(main)/add-new/register/3/page.tsx
@@ -0,0 +1,9 @@
+import { FormNavigation } from "@/components/forms/form-nav";
+export default function FormPage3() {
+  return (
+    <div>
+      <h1>Form Page 3</h1>
+      <FormNavigation />
+    </div>
+  );
+}
