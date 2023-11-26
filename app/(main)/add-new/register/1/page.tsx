c828704e56fb3ff901c9def4db63f76c1ec35e0a
syndg <sinchandsgpt9@gmail.com>
2023-11-26 21:24:50 +0530

    FormPages: Check if the currentUser is the owner of the restaurant

--- app/(main)/add-new/register/1/page.tsx
+++ app/(main)/add-new/register/1/page.tsx
@@ -1,13 +1,26 @@
 import Formstep1 from "@/components/forms/FormStep1";
 import { getRestaurant } from "@/actions/getRestaurant";
+import { auth } from "@clerk/nextjs";
 
 export default async function FormPage({
   searchParams,
 }: {
   searchParams: { [res_id: string]: string };
 }) {
+  const { userId } = auth();
   const { res_id } = searchParams;
   const restaurant = await getRestaurant(res_id ? res_id : "");
+  const isOwner = userId === restaurant?.ownerId;
+
+  if (res_id && !isOwner) {
+    return (
+      <div className="min-h-[80vh] grid place-content-center">
+        <p className="p-3 border border-primary rounded-md">
+          You are not the owner of this restaurant
+        </p>
+      </div>
+    );
+  }
 
   return <Formstep1 initialValues={restaurant} />;
 }
--- app/(main)/add-new/register/2/page.tsx
+++ app/(main)/add-new/register/2/page.tsx
@@ -2,6 +2,7 @@ import { getRestaurant } from "@/actions/getRestaurant";
 import FormStep2 from "@/components/forms/FormStep2";
 import { redirect } from "next/navigation";
 import { getResTypeandTiming } from "@/actions/getRestaurant";
+import { auth } from "@clerk/nextjs";
 
 export default async function FormPage({
   searchParams,
@@ -11,9 +12,20 @@ export default async function FormPage({
   const { res_id } = searchParams;
   const restaurant = await getRestaurant(res_id);
   const resTypeandTiming = await getResTypeandTiming(res_id);
+  const { userId } = auth();
 
   if (!res_id || !restaurant) {
     redirect("/add-new/register/1");
+  }
+
+  if (userId !== restaurant.ownerId) {
+    return (
+      <div className="min-h-[80vh] grid place-content-center">
+        <p className="p-3 border border-primary rounded-md">
+          You are not the owner of this restaurant
+        </p>
+      </div>
+    );
   }
 
   return <FormStep2 resId={res_id} initialValues={resTypeandTiming} />;
