a8ade790fc5a335662546036eef306133df17079
syndg <sinchandsgpt9@gmail.com>
2023-11-29 23:22:41 +0530

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-[100svh] grid place-content-center">
+      <h1 className="text-xl border-2 border-primary p-3 rounded-md">
+        Oops - Page Not Found
+      </h1>
+    </div>
+  );
+}
